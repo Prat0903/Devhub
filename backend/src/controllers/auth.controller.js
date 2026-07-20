944267d6c2d25178b5719bc42c773af6925ce5fd
Prateek <sqonyx56@gmail.com>
2026-07-20 17:22:06 +0530

    controllerupdated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,39 +1,40 @@
 const UserModel = require("../models/user.model");
 let bcrypt = require("bcrypt");
 const generateToken = require("../utils/token");
-const { registerService, loginService } = require("../services/auth.service");
+const {
+  registerService,
+  loginService,
+  logoutService,
+} = require("../services/auth.service");
 const asyncHandler = require("../utils/asyncHandler");
 const ApiResponse = require("../utils/apiResponse");
 
 let registerController = asyncHandler(async (req, res) => {
-  try {
-    let { newUser, token } = await registerService(req.body);
+  let { newUser, token } = await registerService(req.body);
 
-    res.cookie("token", token);
+  res.cookie("token", token);
 
-    return res
-      .status(201)
-      .json(new ApiResponse("User registered successfully", newUser));
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json(new ApiResponse("Internal server error"));
-  }
+  return res
+    .status(201)
+    .json(new ApiResponse("User registered successfully", newUser));
 });
 
-let loginController = async (req, res) => {
-  try {
-    let { isExisting, token } = await loginService(req.body);
+let loginController = asyncHandler(async (req, res) => {
+  let { isExisting, token } = await loginService(req.body);
 
-    return res
-      .status(200)
-      .json(new ApiResponse("User loggedin successfully", isExisting));
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json(new ApiResponse("Internal server error"));
-  }
-};
+  return res
+    .status(200)
+    .json(new ApiResponse("User logged in", isExisting));
+});
+
+let logoutController = asyncHandler(async (req, res) => {
+  res.clearCookie("token");
+
+  return res.status(200).json(new ApiResponse("User logged out"));
+});
 
 module.exports = {
   registerController,
   loginController,
+  logoutController,
 };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,12 +2,13 @@ let express = require("express");
 const {
   registerController,
   loginController,
+  logoutController,
 } = require("../controllers/auth.controller");
 
 let router = express.Router();
 
 router.post("/register", registerController);
-
 router.post("/login", loginController);
+router.post("/logout", logoutController);
 
 module.exports = router;
